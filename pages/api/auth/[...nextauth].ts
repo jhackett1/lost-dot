ffb2bbaf0168ad79570ab96c12bb480254a06847
Jaye Hackett <jaye.hackett@gmail.com>
2022-11-29 23:57:36 +0000

    configure user accounts on db

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
