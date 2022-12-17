e96ed8aa22433124fdfb997620bc3ff0b84835b9
Jaye Hackett <jaye.hackett@gmail.com>
2022-12-17 00:12:04 +0000

    tweaks

--- lib/files.ts
+++ lib/files.ts
@@ -1,4 +1,8 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import {
+  PutObjectCommand,
+  S3Client,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3"
 
 // Step 2: The s3Client function validates your request and directs it to your Space's specified endpoint using the AWS SDK.
 const s3Client = new S3Client({
@@ -11,19 +15,27 @@ const s3Client = new S3Client({
   },
 })
 
-export const getFileByID = (id: string) => {}
+const bucket = "lost-dot-staging"
 
-export const uploadFile = async file => {
+export const getFileByID = async (key: string) =>
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    })
+  )
+
+export const uploadFile = async (key: string, file) => {
   // Step 3: Define the parameters for the object you want to upload.
   const params = {
-    Bucket: "example-space", // The path to the directory you want to upload the object to, starting with your Space name.
-    Key: "folder-path/hello-world.txt", // Object key, referenced whenever you want to access this file later.
-    Body: "Hello, World!", // The object's contents. This variable is an object, not a string.
+    Bucket: bucket, // The path to the directory you want to upload the object to, starting with your Space name.
+    Key: key, // Object key, referenced whenever you want to access this file later.
+    Body: file, // The object's contents. This variable is an object, not a string.
     ACL: "private", // Defines ACL permissions, such as private or public.
-    Metadata: {
-      // Defines metadata tags.
-      "x-amz-meta-my-key": "your-value",
-    },
+    // Metadata: {
+    //   // Defines metadata tags.
+    //   "x-amz-meta-my-key": "your-value",
+    // },
   }
 
   // Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
