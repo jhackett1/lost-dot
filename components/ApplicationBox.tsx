fbcc3644ca66b80a5eecf8bcf2e2821b33ca1450
Jaye Hackett <jaye.hackett@gmail.com>
2022-12-18 23:01:27 +0000

    application box

---
+++ components/ApplicationBox.tsx
@@ -0,0 +1,62 @@
+import { Application } from "@prisma/client"
+import Link from "next/link"
+import { formatDateAndTime } from "../lib/formatters"
+import { Race } from "../types"
+import CompletionMeter from "./CompletionMeter"
+
+const ApplicationBox = ({
+  application,
+  race,
+}: {
+  application: Application
+  race: Race
+}) => {
+  const completion = 0.5
+
+  return (
+    <div className="application-box">
+      <h1 className="application-box__headline">
+        Finish your {race.hashtag} application using the race manual
+      </h1>
+
+      <strong className="application-box__deadline">
+        Deadline: {formatDateAndTime(race.deadline)}
+      </strong>
+
+      <div className="application-box__body">
+        <CompletionMeter completion={completion} />
+
+        <div>
+          <ol className="application-box__timeline">
+            <li className="application-box__timeline-task application-box__timeline-task--complete">
+              <span className="application-box__timeline-complete">
+                Complete:
+              </span>
+              Create a Lost Dot profile
+            </li>
+            <li className="application-box__timeline-task application-box__timeline-task--complete">
+              <span className="application-box__timeline-complete">
+                Complete:
+              </span>
+              Answer a few questions and pay for the race manual
+            </li>
+            <li className="application-box__timeline-task">
+              Use the race manual to answer some specific questions to complete
+              your application
+              <a href="#">View race manual</a>
+            </li>
+          </ol>
+
+          <Link
+            href={`/applications/${application.raceId}/steps`}
+            className="button"
+          >
+            Continue
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ApplicationBox
---
+++ components/CompletionMeter.tsx
@@ -0,0 +1,42 @@
+const CompletionMeter = ({ completion }) => {
+  const size = 200
+  const thickness = 25
+  const radius = size * 0.435
+  const circumference = 2 * Math.PI * radius
+  const strokeOffset = circumference * (1 - completion)
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox={`0 0 ${size} ${size}`}
+      className="completion-meter"
+    >
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        fill="none"
+        stroke-width={thickness}
+      />
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        fill="none"
+        stroke-width={thickness}
+        strokeLinecap="round"
+        stroke-dasharray={circumference}
+        stroke-dashoffset={strokeOffset}
+      />
+      <text x={size / 2} y={(size / 2) * 1.05}>
+        {Math.round(completion * 100)}%
+      </text>
+      <text x={size / 2} y={(size / 2) * 1.3}>
+        complete
+      </text>
+    </svg>
+  )
+}
+
+export default CompletionMeter
