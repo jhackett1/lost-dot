cf72655951762c9a74f356c441526b19f96abbef
Jaye Hackett <jaye.hackett@gmail.com>
2022-12-30 21:29:12 +0000

    set up swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
 import { SessionProvider } from "next-auth/react"
+import { SWRConfig } from "swr"
 import AppLayout from "../components/AppLayout"
 import "../styles/index.scss"
+
+const defaultFetcher = url => fetch(url).then(res => res.json())
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </SessionProvider>
+    <SWRConfig value={{ fetcher: defaultFetcher }}>
+      <SessionProvider session={session}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </SessionProvider>
+    </SWRConfig>
   )
 }
